513cf7db775c1be6d966020c9c0858c42faceb0b
ezequielGoy <ezequielgoy.9@gmail.com>
2025-04-01 19:57:20 -0300

    creo sv vinculo mongodb

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB connected'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}`);
+  });
+  
